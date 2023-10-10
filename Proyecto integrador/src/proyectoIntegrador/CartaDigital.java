e3eb3cbceb82c486a51ab7e0d89864b7eef8de97
Renzo Arredondo <renzo.mau1988@gmail.com>
2023-10-09 21:55:06 -0300

    Se genera constructor y atributos

---
+++ Proyecto integrador/src/proyectoIntegrador/CartaDigital.java
@@ -0,0 +1,31 @@
+package proyectoIntegrador;
+
+public class CartaDigital {
+
+	//Definimos los atributos de la clase, o caracteristicas. Basicamente son todas las variables que contendra el menu
+	 	private String[] entradas = {"Ensalada","Empanada","Bruschetta","Sopa"};
+	    private double[] preciosEntradas = {1250,700,1450,1900};
+	    private int [] cantidadEntrada;
+	    
+	    private String[] platosPrincipales = {"Milanesa","Lomito","Filette de salmon","Sorrentinos"};
+	    private double[] preciosPlatosPrincipales = {3400,4100,6400,2900};
+	    private int [] cantidadPlatosPrincipales;
+	    
+	    private String[] postres = {"Flan","Bombon escoces","Ensalada de frutas","Tiramisu"};
+	    private double[] preciosPostres={1300,1500,900,2000};
+	    private int [] cantidadPostres;
+	    
+	    private String[] bebidas = {"Agua sin Gas","Gaseosa","Vino de la casa","Cerveza"};
+	    private double[] preciosBebidas={900,1000,2100,1200};
+	    private int [] cantidadBebidas;
+	
+	
+	
+	//Armamos el constructor
+	
+	public CartaDigital() {
+		
+		}
+	
+	
+}
---
+++ Proyecto integrador/src/proyectoIntegrador/UsoCartaDigital.java
@@ -0,0 +1,18 @@
+package proyectoIntegrador;
+
+public class UsoCartaDigital {
+
+	public static void main(String[] args) {
+	
+	
+		CartaDigital menuEntrada = new CartaDigital();//instanciamos objeto menu entrada
+		CartaDigital menuPlatoPrincipal= new CartaDigital();
+		CartaDigital menuPostre = new CartaDigital();
+		CartaDigital menuBebida = new CartaDigital();
+		
+
+	}
+
+	
+
+}
