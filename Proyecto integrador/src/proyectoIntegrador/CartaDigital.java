dee506fb87df5dc96d208201de2b1b2db5bf446a
Renzo Arredondo <renzo.mau1988@gmail.com>
2023-10-10 21:25:51 -0300

    Se modifica lo iniciado y se avanza submenu entrada

--- Proyecto integrador/src/proyectoIntegrador/CartaDigital.java
+++ Proyecto integrador/src/proyectoIntegrador/CartaDigital.java
@@ -3,11 +3,12 @@ package proyectoIntegrador;
 public class CartaDigital {
 
 	//Definimos los atributos de la clase, o caracteristicas. Basicamente son todas las variables que contendra el menu
-	 	private String[] entradas = {"Ensalada","Empanada","Bruschetta","Sopa"};
-	    private double[] preciosEntradas = {1250,700,1450,1900};
+	 	
+		private String [] entradas;
+		private double[] preciosEntradas ;
 	    private int [] cantidadEntrada;
 	    
-	    private String[] platosPrincipales = {"Milanesa","Lomito","Filette de salmon","Sorrentinos"};
+	  /*  private String[] platosPrincipales = {"Milanesa","Lomito","Filette de salmon","Sorrentinos"};
 	    private double[] preciosPlatosPrincipales = {3400,4100,6400,2900};
 	    private int [] cantidadPlatosPrincipales;
 	    
@@ -17,7 +18,7 @@ public class CartaDigital {
 	    
 	    private String[] bebidas = {"Agua sin Gas","Gaseosa","Vino de la casa","Cerveza"};
 	    private double[] preciosBebidas={900,1000,2100,1200};
-	    private int [] cantidadBebidas;
+	    private int [] cantidadBebidas;*/
 	
 	
 	
@@ -25,7 +26,33 @@ public class CartaDigital {
 	
 	public CartaDigital() {
 		
+	entradas= new String[] {"Ensalada","Empanada","Bruschetta","Sopa"};
+	preciosEntradas=new double [] {1250,700,1450,1900};
+		
 		}
 	
 	
+public String dimeProductoPrecio() { //GETTER Al llamar este metodo nos mostrará los datos precargados en el constructor
+		
+	for (int i = 0; i < entradas.length; i++) {
+        System.out.println((i+1)+" - "+entradas[i] + "......... $" + preciosEntradas[i]); 
+				
+	}
+	return "Menu de entradas";
+	}
+
+public void estableceCantidades() {
+	
+	while (i !=0) {
+		
+		
+		
+	}
 }
+
+
+}
+
+	
+	
+	
--- Proyecto integrador/src/proyectoIntegrador/UsoCartaDigital.java
+++ Proyecto integrador/src/proyectoIntegrador/UsoCartaDigital.java
@@ -3,14 +3,17 @@ package proyectoIntegrador;
 public class UsoCartaDigital {
 
 	public static void main(String[] args) {
-	
+		
+		
+		
 	
 		CartaDigital menuEntrada = new CartaDigital();//instanciamos objeto menu entrada
-		CartaDigital menuPlatoPrincipal= new CartaDigital();
+		/*CartaDigital menuPlatoPrincipal= new CartaDigital();
 		CartaDigital menuPostre = new CartaDigital();
 		CartaDigital menuBebida = new CartaDigital();
-		
+		*/
 
+		menuEntrada.dimeProductoPrecio();
 	}
 
 	
