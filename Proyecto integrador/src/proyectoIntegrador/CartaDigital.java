088143b3f326ae97a43247cd2e97db93b2d0a58f
Renzo Arredondo <renzo.mau1988@gmail.com>
2023-10-14 20:32:39 -0300

    Se finaliza submenu entrada

--- Proyecto integrador/src/proyectoIntegrador/CartaDigital.java
+++ Proyecto integrador/src/proyectoIntegrador/CartaDigital.java
@@ -1,6 +1,6 @@
 package proyectoIntegrador;
-import javax.swing.JOptionPane;
 import java.util.Scanner;
+
 
 public class CartaDigital {
 
@@ -8,7 +8,7 @@ public class CartaDigital {
 	 	
 		private String [] entradas;
 		private double[] preciosEntradas ;
-	    private int [] cantidadEntrada;
+		int[] cantidadEntrada ;
 	    
 	  /*  private String[] platosPrincipales = {"Milanesa","Lomito","Filette de salmon","Sorrentinos"};
 	    private double[] preciosPlatosPrincipales = {3400,4100,6400,2900};
@@ -31,17 +31,19 @@ public class CartaDigital {
 		entradas= new String[] {"Ensalada","Empanada","Bruschetta","Sopa"};
 		preciosEntradas=new double [] {1250,700,1450,1900};
 		
-		}
+	}
 	
 	
 public String dimeProductoPrecio() { //GETTER Al llamar este metodo nos mostrará los datos precargados en el constructor
-		
+	
+	System.out.println(" ------------ MENU ENTRADAS -----------");
+	
 	for (int i = 0; i < entradas.length; i++) {
         System.out.println((i+1)+" - "+entradas[i] + "......... $" + preciosEntradas[i]); 
 				
 	}
-	
-	System.out.println("Elige una opcion"); 
+	System.out.println("0 - Volver al menu principal");
+
 	
 	return "Menu de entradas";
 	}
@@ -51,45 +53,68 @@ public void cantidadEntrada() {//Setter establece cantidades entrada
 	
 	
 	int opcionEntrada;
-	Scanner opcionEntradaa=new Scanner(System.in);
-	opcionEntrada=Integer.parseInt(opcionEntradaa.nextLine());
+	cantidadEntrada=new int [4];
 	
-	   switch (opcionEntrada) {
-	   
+	Scanner opcionEntradaBis=new Scanner(System.in);//esta opcion de consola elige una de las 4 opciones de entrada
+	Scanner opcionCantidadBis=new Scanner(System.in);//Esta opcion de consola settea cantidades
+	
+	System.out.println("Indique una opcion: ");
+	opcionEntrada=Integer.parseInt(opcionEntradaBis.nextLine());
+	
+	
+	
+	do {
+		
+	
+	
+	switch (opcionEntrada) //Segun la opcion que elija, sera la posicion dela rray cantidadEntrada[] que se llene
+	   						{
+	      
 	   case 1:
-           System.out.println("Has elegido "+entradas[0]);
-           cantidadEntrada[opcionEntrada-1]=Integer.parseInt(JOptionPane.showInputDialog("Indique cantidad"));
-           break;
+           System.out.println("Has elegido "+entradas[0]+". Indique cantidad: ");
+           cantidadEntrada[0]=Integer.parseInt(opcionCantidadBis.nextLine());
+           System.out.println("Usted ha seleccionado "+entradas[0]+" por "+cantidadEntrada[0]+" unidades.");
+           dimeProductoPrecio();
+           cantidadEntrada();
            
        case 2:
-    	   System.out.println("Has elegido "+entradas[1]);
-           cantidadEntrada[opcionEntrada-1]=Integer.parseInt(JOptionPane.showInputDialog("Indique cantidad"));
-           break;
+           System.out.println("Has elegido "+entradas[1]+". Indique cantidad: ");
+           cantidadEntrada[1]=Integer.parseInt(opcionCantidadBis.nextLine());
+           System.out.println("Usted ha seleccionado "+entradas[1]+" por "+cantidadEntrada[1]+" unidades.");
+           dimeProductoPrecio();
+           cantidadEntrada();
            
        case 3:
-    	   System.out.println("Has elegido "+entradas[2]);
-           cantidadEntrada[opcionEntrada-1]=Integer.parseInt(JOptionPane.showInputDialog("Indique cantidad"));
-           break;
+    	   System.out.println("Has elegido "+entradas[2]+". Indique cantidad: ");
+           cantidadEntrada[2]=Integer.parseInt(opcionCantidadBis.nextLine());
+           System.out.println("Usted ha seleccionado "+entradas[2]+" por "+cantidadEntrada[2]+" unidades.");
+           dimeProductoPrecio();
+           cantidadEntrada();
            
        case 4:
-    	   System.out.println("Has elegido "+entradas[3]);
-           cantidadEntrada[opcionEntrada-1]=Integer.parseInt(JOptionPane.showInputDialog("Indique cantidad"));
-           break;
+    	   System.out.println("Has elegido "+entradas[3]+". Indique cantidad: ");
+           cantidadEntrada[3]=Integer.parseInt(opcionCantidadBis.nextLine());
+           System.out.println("Usted ha seleccionado "+entradas[3]+" por "+cantidadEntrada[3]+" unidades.");
+           dimeProductoPrecio();
+           cantidadEntrada();
+              
        
        case 0:
            // Salir del bucle si se elige la opción 0
            System.out.println("Volviendo al menu principal");
           
            System.exit(0);
-	   
+           
+       default:
+           System.out.println("Opción no válida. Por favor, elige del 0 al 5.");
+           dimeProductoPrecio();
+           cantidadEntrada();
 	   }
-       
+	
+	} while(opcionEntrada !=0);
 	
 	
 }
-
-
-
 
 
 
